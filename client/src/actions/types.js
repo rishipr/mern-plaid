69d85723875223cc18341f2a6fdc9f5327c41261
Rishi Prasad <rishipr@umich.edu>
2019-01-01 12:12:03 -0500

    Removed unnecessary current user API route from mern-auth repo

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,7 +1,6 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
-export const GET_CURRENT_USER = "GET_CURRENT_USER";
 export const ADD_ACCOUNT = "ADD_ACCOUNT";
 export const DELETE_ACCOUNT = "DELETE_ACCOUNT";
 export const GET_ACCOUNTS = "GET_ACCOUNTS";
